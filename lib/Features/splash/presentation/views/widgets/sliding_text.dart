cb3a283b11e4bcfadc651d21f4810bd3d09ebce0
UAS <aa282277@gmail.com>
2024-09-14 07:51:28 +0300

    +sliding animation to splash view

---
+++ lib/Features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        //to make the animation widget [only] that well be updated(rebuild)
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read Free Books',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          );
+        });
+  }
+}
--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,24 +1,75 @@
+import 'package:booklo/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:booklo/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+    //startAnimation();
+
+    animationController.forward();
+
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    // to avoid memory leaks, to dispose the animation controller
+    // TODO: implement dispose
+    super.dispose();
+
+    animationController
+        .dispose(); // every controller => must be disposed to avoid memory leaks
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment:
-          CrossAxisAlignment.stretch, //عشان ال شايلد ياخد اكبر عرض ممكن ياخدو
+          CrossAxisAlignment.stretch, //عشان ال تشايلد ياخد اكبر عرض ممكن ياخدو
       children: [
-        Image.asset(AssetsData.logo),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AssetsData.logo,
+              fit: BoxFit.fill,
+              height: 70,
+              width: 290,
+            ),
+          ],
+        ),
         const SizedBox(
-          height: 5,
+          height: 10,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
