863504f8fac17a5d5129b159f8aed1d2fe2588c4
UAS <aa282277@gmail.com>
2024-09-16 03:44:01 +0300

    +CustomListViewItem to homeView

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,16 +1,16 @@
 import 'package:booklo/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:booklo/core/utils/assets.dart';
+import 'package:booklo/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
+    return const Column(
+      children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
